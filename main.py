d0c1fde9528c03d2fc2a61b328d75ba84d10eb38
Alper Canberk Balcı <a.canberk.balci@gmail.com>
2021-02-28 21:00:39 +0300

    loss functions

--- main.py
+++ main.py
@@ -24,15 +24,34 @@ class MyDenseLayer(tf.keras.layers.Layer):
 # layer, so we don't need to do that from scratch
 # instead, we can just call like this:
 # 2 outputs.
-layer = tf.keras.layers.Dense(units=2)
-n1 = 5
-n2 = 65
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(n1),
-    tf.keras.layers.Dense(n2),
-    #
-    # hidden layers
-    #
-    tf.keras.layers.Dense(2)  # 2 output neurons.
-])
 
+# layer = tf.keras.layers.Dense(units=2)
+
+# n1 = 5
+# n2 = 65
+# model = tf.keras.Sequential([
+#    tf.keras.layers.Dense(n1),
+#    tf.keras.layers.Dense(n2),
+#    #
+#    # hidden layers
+#    #
+#    tf.keras.layers.Dense(2)  # 2 output neurons.
+# ])
+# #################################### #
+# Example Problem: Will I pass this class? Simple two feature model
+# Data: x1 := number of lectures I attended.
+#       x2 := hours spent on the final project.
+
+# My x vector iz [4, 5]. But this is not enough to train a model. I need data of a lot of students.
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(3),
+    tf.keras.layers.Dense(2)
+])
+# This is a binary classification problem. We can use what is called a soft max cross entropy loss
+# cross entropy bw/ two prob. distr., it measures how far apart the ground truth prob. distr. is
+# from the predicted prob. distr.
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, predicted))
+# If I want to find out the grade, I will use another loss func. because the output type changed.
+# Mean Squared Error Loss
+loss = tf.reduce_mean(tf.square(tf.subtract(y, predicted))) # OR...
+loss = tf.keras.losses.MSE(y, predicted)
